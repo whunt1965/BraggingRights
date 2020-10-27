b2153e3f0f57285459820dc5fadfb695f9c97b47
Wiley Hunt <johnhunt@MacBook-Pro.local>
2020-10-27 18:26:31 -0400

    Sorted inputs to match real app behavior

--- test_PrintResults.py
+++ test_PrintResults.py
@@ -1,7 +1,7 @@
 import PrintResults as PR
 
-coaches = [{"handle": "@Mickey", "score": 2, "mostpos": "I love Mickey", "mostneg": "I hate Mickey"},\
-{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"},\
+coaches = [{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"},\
+{"handle": "@Mickey", "score": 2, "mostpos": "I love Mickey", "mostneg": "I hate Mickey"},\
 {"handle": "@Goofy", "score": 9, "mostpos": "I love Goofy", "mostneg": "I hate Goofy"}]
 
 def test_HC(capfd):
