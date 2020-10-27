d30e0a7f58f2a9829b5dde7a66c2c8dc2bd5f800
Wiley Hunt <johnhunt@MacBook-Pro.local>
2020-10-27 17:28:39 -0400

    Attempting to fix custom list test

--- test_customlists.py
+++ test_customlists.py
@@ -1,9 +1,8 @@
 import CustomList as cl
 from io import StringIO
 
-myinput = StringIO('hello')
+myinput = StringIO('hello', '0')
 
 def test_input(monkeypatch):
     monkeypatch.setattr('sys.stdin', myinput)
-    monkeypatch.setattr('sys.stdin', 0)
     assert(cl.getCustomList()) == ['@hello']
