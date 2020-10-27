32049d73fc6f69c5226d9b80c255185b03ff957d
Wiley Hunt <johnhunt@MacBook-Pro.local>
2020-10-27 18:30:06 -0400

    Fixing annoying spacing issues killing test

--- test_PrintResults.py
+++ test_PrintResults.py
@@ -8,19 +8,19 @@ def test_HC(capfd):
     resultHC = """*** Who's on the Hot Seat?! ***
 
     1: Donald (On the Hot Seat!)
-        Score: 0
-        Most Positive Tweet: Donald is the Best 
-        Most Negative Tweet: I hate Donald
+         Score: 0
+         Most Positive Tweet: Donald is the Best 
+         Most Negative Tweet: I hate Donald
 
     2: Mickey
-        Score: 2
-        Most Positive Tweet: I love Mickey 
-        Most Negative Tweet: I hate Mickey
+         Score: 2
+         Most Positive Tweet: I love Mickey 
+         Most Negative Tweet: I hate Mickey
 
     3. Goofy (Looks Safe! For now...)
-        Score: 9
-        Most Positive Tweet: I love Goofy 
-        Most Negative Tweet: I hate Goofy
+         Score: 9
+         Most Positive Tweet: I love Goofy
+         Most Negative Tweet: I hate Goofy
 
     """
     msg = "*** Who's on the Hot Seat?! ***"
