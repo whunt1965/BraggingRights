e71354f7e4947acad7f2940c9da498e91c119db5
Wiley Hunt <johnhunt@MacBook-Pro.local>
2020-10-27 17:39:09 -0400

    Added Additional Lists

--- test_customlists.py
+++ test_customlists.py
@@ -1,8 +1,19 @@
 import CustomList as cl
 from io import StringIO
 
-myinput = StringIO('hello\n0\n')
+myinput0 = StringIO('0\n')
+myinput1 = StringIO('hello\n0\n')
+myinputmany = StringIO('Obama\nBush1\nBush2\nClinton\n0\n')
 
-def test_input(monkeypatch):
-    monkeypatch.setattr('sys.stdin', myinput)
+def test_input0(monkeypatch):
+    monkeypatch.setattr('sys.stdin', myinput0)
+    assert(cl.getCustomList()) == []
+
+def test_input1(monkeypatch):
+    monkeypatch.setattr('sys.stdin', myinput1)
     assert(cl.getCustomList()) == ['@hello']
+
+
+def test_inputmany(monkeypatch):
+    monkeypatch.setattr('sys.stdin', myinputmany)
+    assert(cl.getCustomList()) == ['@Obama', '@Bush1','@Bush2','@Clinton']
