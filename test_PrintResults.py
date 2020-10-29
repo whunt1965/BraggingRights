ff5fa8ffc0b1dff1c5bfc3af74e61fb282d5eccd
Wiley Hunt <johnhunt@MacBook-Pro.local>
2020-10-29 13:12:54 -0400

    Added new tests

--- test_PrintResults.py
+++ test_PrintResults.py
@@ -1,10 +1,13 @@
 import PrintResults as PR
 
-results = [{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"},\
+
+#Heat Check Tests****
+
+HCresults1 = [{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"},\
 {"handle": "@Mickey", "score": 2, "mostpos": "I love Mickey", "mostneg": "I hate Mickey"},\
 {"handle": "@Goofy", "score": 9, "mostpos": "I love Goofy", "mostneg": "I hate Goofy"}]
 
-def test_HC(capfd):
+def test_HC1(capfd):
     resultHC = """*** Who's on the Hot Seat?! ***
 
 1: @Donald (On the Hot Seat!)
@@ -24,11 +27,57 @@ def test_HC(capfd):
 
 """
     msg = "*** Who's on the Hot Seat?! ***"
-    PR.printHeatCheck(msg, results)
+    PR.printHeatCheck(msg, HCresults1)
     out, err = capfd.readouterr()
     assert out == resultHC
 
-def testMI(capfd):
+
+HCresults2 = [{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"}]
+
+def test_HC2(capfd):
+    resultHC = """*** Who's on the Hot Seat?! ***
+
+1: @Donald (On the Hot Seat!)
+     Score: 0
+     Most Positive Tweet: Donald is the Best
+     Most Negative Tweet: I hate Donald
+
+"""
+    msg = "*** Who's on the Hot Seat?! ***"
+    PR.printHeatCheck(msg, HCresults2)
+    out, err = capfd.readouterr()
+    assert out == resultHC
+
+HCresults3 = [{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"},\
+{"handle": "@Mickey", "score": 2, "mostpos": "I love Mickey", "mostneg": "I hate Mickey"}]
+
+def test_HC3(capfd):
+    resultHC = """*** Who's on the Hot Seat?! ***
+
+1: @Donald (On the Hot Seat!)
+     Score: 0
+     Most Positive Tweet: Donald is the Best
+     Most Negative Tweet: I hate Donald
+
+2: @Mickey (Looks Safe! For now...)
+     Score: 2
+     Most Positive Tweet: I love Mickey
+     Most Negative Tweet: I hate Mickey
+
+"""
+    msg = "*** Who's on the Hot Seat?! ***"
+    PR.printHeatCheck(msg, HCresults3)
+    out, err = capfd.readouterr()
+    assert out == resultHC
+
+
+# Misery Index Tests****
+
+MIresults1 = [{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"},\
+{"handle": "@Mickey", "score": 2, "mostpos": "I love Mickey", "mostneg": "I hate Mickey"},\
+{"handle": "@Goofy", "score": 9, "mostpos": "I love Goofy", "mostneg": "I hate Goofy"}]
+
+def testMI1(capfd):
 
     resultMI = """*** Misery Index!! ***
 
@@ -49,16 +98,61 @@ def testMI(capfd):
 
 """
     msg = "*** Misery Index!! ***"
-    PR.PrintMiseryIndex(msg, results)
+    PR.PrintMiseryIndex(msg, MIresults1)
     out, err = capfd.readouterr()
     assert out == resultMI
 
 
-newresults = [{"handle": "@Goofy", "score": 9, "mostpos": "I love Goofy", "mostneg": "I hate Goofy"},\
+MIresults2 = [{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"}]
+
+def testMI2(capfd):
+
+    resultMI = """*** Misery Index!! ***
+
+1: @Donald (Most Miserable!)
+     Score: 0
+     Most Positive Tweet: Donald is the Best
+     Most Negative Tweet: I hate Donald
+
+"""
+    msg = "*** Misery Index!! ***"
+    PR.PrintMiseryIndex(msg, MIresults1)
+    out, err = capfd.readouterr()
+    assert out == resultMI
+
+
+MIresults3 = [{"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"},\
+{"handle": "@Mickey", "score": 2, "mostpos": "I love Mickey", "mostneg": "I hate Mickey"}]
+
+def testMI3(capfd):
+
+    resultMI = """*** Misery Index!! ***
+
+1: @Donald (Most Miserable!)
+     Score: 0
+     Most Positive Tweet: Donald is the Best
+     Most Negative Tweet: I hate Donald
+
+2: @Mickey (Least Miserable! For now...)
+     Score: 2
+     Most Positive Tweet: I love Mickey
+     Most Negative Tweet: I hate Mickey
+
+"""
+    msg = "*** Misery Index!! ***"
+    PR.PrintMiseryIndex(msg, MIresults1)
+    out, err = capfd.readouterr()
+    assert out == resultMI
+
+
+
+# Custom Rank Tests****
+
+CRresults1 = [{"handle": "@Goofy", "score": 9, "mostpos": "I love Goofy", "mostneg": "I hate Goofy"},\
 {"handle": "@Mickey", "score": 2, "mostpos": "I love Mickey", "mostneg": "I hate Mickey"},\
 {"handle": "@Donald", "score": 0, "mostpos": "Donald is the Best", "mostneg": "I hate Donald"},]
 
-def testCR(capfd):
+def testCR1(capfd):
     resultCR = """Your Ranking Results! (Most Positive to Least Positive)
 
 1: @Goofy (Most Positive!)
@@ -78,6 +172,46 @@ def testCR(capfd):
 
 """
 
-    PR.PrintCustomRank(newresults)
+    PR.PrintCustomRank(CRresults1)
+    out, err = capfd.readouterr()
+    assert out == resultCR
+
+
+CRresults2 = [{"handle": "@Goofy", "score": 9, "mostpos": "I love Goofy", "mostneg": "I hate Goofy"}]
+
+def testCR2(capfd):
+    resultCR = """Your Ranking Results! (Most Positive to Least Positive)
+
+1: @Goofy (Most Positive!)
+     Score: 9
+     Most Positive Tweet: I love Goofy
+     Most Negative Tweet: I hate Goofy
+
+"""
+
+    PR.PrintCustomRank(CRresults1)
+    out, err = capfd.readouterr()
+    assert out == resultCR
+
+
+CRresults3 = [{"handle": "@Goofy", "score": 9, "mostpos": "I love Goofy", "mostneg": "I hate Goofy"},\
+{"handle": "@Mickey", "score": 2, "mostpos": "I love Mickey", "mostneg": "I hate Mickey"}]
+
+def testCR1(capfd):
+    resultCR = """Your Ranking Results! (Most Positive to Least Positive)
+
+1: @Goofy (Most Positive!)
+     Score: 9
+     Most Positive Tweet: I love Goofy
+     Most Negative Tweet: I hate Goofy
+
+2: @Mickey (Most Negative!)
+     Score: 2
+     Most Positive Tweet: I love Mickey
+     Most Negative Tweet: I hate Mickey
+     
+"""
+
+    PR.PrintCustomRank(CRresults1)
     out, err = capfd.readouterr()
     assert out == resultCR
